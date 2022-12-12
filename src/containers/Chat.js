b392614b75cb630bfe733fc55943633a79f5d0ef
phamdai <daipham952@gmail.com>
2022-12-12 14:07:52 +0700

    first-commit

--- src/containers/Chat.js
+++ src/containers/Chat.js
@@ -417,7 +417,7 @@ const Message=(props)=>{
     }
 
     useEffect(() => {
-        socket.current=io.connect('https://anhdai12345.herokuapp.com/');
+        socket.current=io.connect('https://web-production-e133.up.railway.app/');
         socket.current.on('message',({data})=>{ 
             if(data.typing || data.typing==""){
                 if(data.typing==""){
