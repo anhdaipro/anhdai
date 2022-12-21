a26e71100e0b64b1e23e218e397b71112477ace4
phamdai <daipham952@gmail.com>
2022-12-21 16:28:19 +0700

    first-commit

--- src/user/Purchase.js
+++ src/user/Purchase.js
@@ -42,7 +42,7 @@ const Infoitem=(props)=>{
                     </div>
                 </div>
                 <div className="_1kvNGb">
-                    <span className={`${item.discount_price==0?'mBERmM':'_34DOjq'}`}>₫{formatter.format(item.price)}</span>
+                    <span className={`${!item.discount_price?'mBERmM':'_34DOjq'}`}>₫{formatter.format(item.price)}</span>
                     {item.discount_price>0?<span className="mBERmM _2mEJ4q">₫{item.price-item.discount_price}</span>:''}
                 </div>
             </span>
