5cc3c57c0ddffe22b4e2299e467b49a897eac5c6
phamdai <daipham952@gmail.com>
2022-12-22 08:38:38 +0700

    first-commit

--- src/containers/Categorydetail.js
+++ src/containers/Categorydetail.js
@@ -289,7 +289,7 @@ const Categorydetail = ({data,category_id}) => {
                                     }
                                     {categories.category_children.length>2?
                                     <div className="stardust-dropdown folding-items__toggle">
-                                        {showmore.children && (<div className="stardust-dropdown__item-header">
+                                        {!showmore.children && (<div className="stardust-dropdown__item-header">
                                             <div onClick={()=>setShowmore({...showmore,children:true})} className="filter-group__toggle-btn">
                                                 Thêm<svg enableBackground="new 0 0 11 11" viewBox="0 0 11 11" x="0" y="0" className="svg-icon icon-arrow-down"><g><path d="m11 2.5c0 .1 0 .2-.1.3l-5 6c-.1.1-.3.2-.4.2s-.3-.1-.4-.2l-5-6c-.2-.2-.1-.5.1-.7s.5-.1.7.1l4.6 5.5 4.6-5.5c.2-.2.5-.2.7-.1.1.1.2.3.2.4z"></path></g></svg>
                                             </div>
@@ -342,7 +342,7 @@ const Categorydetail = ({data,category_id}) => {
                             })}
                             {categories.category_choice.length>2?
                             <div className="stardust-dropdown folding-items__toggle">
-                                {showmore.choice && (<div className="stardust-dropdown__item-header">
+                                {!showmore.choice && (<div className="stardust-dropdown__item-header">
                                     <div onClick={()=>setShowmore({...showmore,choice:true})} className="filter-group__toggle-btn">
                                         Thêm<svg enableBackground="new 0 0 11 11" viewBox="0 0 11 11" x="0" y="0" className="svg-icon icon-arrow-down"><g><path d="m11 2.5c0 .1 0 .2-.1.3l-5 6c-.1.1-.3.2-.4.2s-.3-.1-.4-.2l-5-6c-.2-.2-.1-.5.1-.7s.5-.1.7.1l4.6 5.5 4.6-5.5c.2-.2.5-.2.7-.1.1.1.2.3.2.4z"></path></g></svg>
                                     </div>
