e2c55f42b5f0fa046001b25f0e5e355ffa42455f
phamdai <daipham952@gmail.com>
2022-12-22 10:13:41 +0700

    first-commit

--- src/containers/Categorydetail.js
+++ src/containers/Categorydetail.js
@@ -323,7 +323,7 @@ const Categorydetail = ({data,category_id}) => {
                         <svg enableBackground="new 0 0 15 15" viewBox="0 0 15 15" x="0" y="0" className="svg-icon "><g><polyline fill="none" points="5.5 13.2 5.5 5.8 1.5 1.2 13.5 1.2 9.5 5.8 9.5 10.2" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10"></polyline></g></svg>
                         <div className="search-filter-status__text">Bộ lọc tìm kiếm</div>
                     </div>
-                    <div className="filter-group facet-filter">
+                    {category_choice.length>0 &&(<div className="filter-group facet-filter">
                         <div className="filter-group__header">Theo Danh Mục</div>
                         <div className="folding-items filter-group__body folding-items--folded">
                             {category_choice.map((category,index)=>{
@@ -377,7 +377,7 @@ const Categorydetail = ({data,category_id}) => {
                                 </div>
                             </div>:''}
                         </div>
-                    </div>
+                    </div>)}
                     {listitem?
                     <div className="filter-group location-filter">
                         <div className="filter-group__header">Nơi Bán</div>
