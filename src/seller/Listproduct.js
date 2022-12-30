aa316e0542b2e49b10929afa06d4687dcca40d6c
phamdai <daipham952@gmail.com>
2022-12-30 10:08:14 +0700

    first-commit

--- src/seller/Listproduct.js
+++ src/seller/Listproduct.js
@@ -503,7 +503,7 @@ const Listproduct=()=>{
                                                 <span onClick={()=>setState({...state,show:!state.show})} className="pagination-sizes__content">{currentPage.pagesize}/pages
                                                     <i className="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M8,9.18933983 L4.03033009,5.21966991 C3.73743687,4.9267767 3.26256313,4.9267767 2.96966991,5.21966991 C2.6767767,5.51256313 2.6767767,5.98743687 2.96966991,6.28033009 L7.46966991,10.7803301 C7.76256313,11.0732233 8.23743687,11.0732233 8.53033009,10.7803301 L13.0303301,6.28033009 C13.3232233,5.98743687 13.3232233,5.51256313 13.0303301,5.21966991 C12.7374369,4.9267767 12.2625631,4.9267767 11.9696699,5.21966991 L8,9.18933983 Z"></path></svg></i>
                                                 </span>  
-                                                <div className="popper pagination-sizes__popper" style={{display: `${state.show?'':'none'}`}}>
+                                                {itemshop.count_product>12&&(<div className="popper pagination-sizes__popper" style={{display: `${state.show?'':'none'}`}}>
                                                     <ul className="dropdown-menu" style={{maxWidth: '440px'}}>
                                                         {pagesize.map(item=>{
                                                             if(item<itemshop.count_product){
@@ -517,7 +517,7 @@ const Listproduct=()=>{
                                                             }
                                                         )}
                                                     </ul>
-                                                </div>
+                                                </div>)}
                                             </div>
                                         </div>
                                     </div>:''}
