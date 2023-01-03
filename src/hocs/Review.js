e65c73fcda957da4cd40bb3e39299478c14d6b55
phamdai <daipham952@gmail.com>
2023-01-03 09:27:11 +0700

    first-commit

--- src/hocs/Review.js
+++ src/hocs/Review.js
@@ -4,9 +4,7 @@ import { headers } from '../actions/auth';
 import {formatter,itemvariation,hidestring,list_review_text_star,
 star_solid,star_empty,dataURLtoFile,rating_score,list_rating_category_bab,list_reason_cancel} from "../constants"
 import {threadlURL,purchaselistdURL,localhost, reviewURL,} from "../urls"
-const [
-    
-]
+
 const Listreview=({order_choice,cancel,list_orders,setcancel,show,list_review,user,setshow,updateorder,
     edit,setedit,list_cartitem,setcartitem,setChoice,setlistreview})=>{
     const [state, setState] = useState({loading:false,review:null,submit:false,
